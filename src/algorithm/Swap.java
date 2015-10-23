c1616567bc48f280261bd40f717b7ba4faf0430e
ggrivera <ggrivera@310A31411.district.local>
2015-10-23 10:18:59 -0500

    finish arraylist and array

--- src/algorithm/Swap.java
+++ src/algorithm/Swap.java
@@ -6,59 +6,73 @@ public class Swap
 {
 	private static int x = 1;
     private static int y = 2;
-    //private static ArrayList<Integer> q;
+    private static int x1 = 1;
+    private static int y1 = 2;
+    private static ArrayList<Integer> arraylist;
     
     
 	public static void main(String[] args) 
 	{
 		Swap swap = new Swap();
-		//ArrayList<Integer> q = new ArrayList();
+		arraylist = new ArrayList<Integer>();
+	    arraylist.add(1);
+	    arraylist.add(0);
 		
 		//swap static
         System.out.println("Before swaping static:");
         System.out.println("x value: "+x);
-        System.out.println("y value: "+y);
-        
-        swap.swapStatics();
-        
+        System.out.println("y value: "+y);        
+        swap.swapStatics();        
         System.out.println("After swaping static:");
         System.out.println("x value: "+x);
         System.out.println("y value: "+y);
         
+        System.out.println("");
+        System.out.println("");
+        
         //swap local par 
         System.out.println("Before swaping swapLocalPar:");
-        System.out.println("Before swap:");
-        System.out.println("x value: "+x);
-        System.out.println("y value: "+y);
+        System.out.println("x value: "+x1);
+        System.out.println("y value: "+y1);
+        System.out.println("After swaping swaplocalPar:");
+        System.out.println(swap.swapLocalPar(x1, y1));     
         
-        swap.swapLocalPar(x, y);
-        
-        System.out.println("After swaping swapLocalPar:");
-        System.out.println("x value: "+x);
-        System.out.println("y value: "+y);
+        System.out.println("");
+        System.out.println("");
         
         //swaplocalpar2
         System.out.println("Before swaping swapLocalPar2:");
-        System.out.println("Before swap:");
         System.out.println("x value: "+x);
-        System.out.println("y value: "+y);
-        
-        swap.swapLocalPar2(x, y);
-        
+        System.out.println("y value: "+y);        
+        swap.swapLocalPar2(x, y);        
         System.out.println("After swaping swapLocalPar2:");
         System.out.println("x value: "+x);
         System.out.println("y value: "+y);
         
+        System.out.println("");
+        System.out.println("");
+        
         System.out.println("Before swaping swapArray:");
-        System.out.println("Before swap:");
         System.out.println("x value: "+x);
-        System.out.println("y value: "+y);
-        
-        //swap.swapArray(a[], x, x);
-        
+        System.out.println("y value: "+y);        
+        //swap.swapArray(a[], x, x);        
         System.out.println("After swaping swapArray:");
         System.out.println("x value: "+x);
         System.out.println("y value: "+y);
+        
+        System.out.println("");
+        System.out.println("");
+        
+        //arraylist swap
+        System.out.println("Before swaping swapList:");
+        System.out.println("x value: "+arraylist.get(0));
+        System.out.println("y value: "+arraylist.get(1));        
+        swap.swapList(arraylist,arraylist.get(0), arraylist.get(1));        
+        System.out.println("After swaping swapList:");
+        System.out.println("x value: "+x);
+        System.out.println("y value: "+y);
+        
+        
 	}
 	
 	public void swapStatics()
@@ -69,14 +83,14 @@ public class Swap
 		 y = temp;
 	}
 	
-	 public String swapLocalPar(int x, int y)
+	 public String swapLocalPar(int x1, int y1)
 	 {
 		 int temp;
-		 temp = x;
-		 x = y;
-		 y = temp;  //FIXME here didnt work?
+		 temp = x1;
+		 x1 = y1;
+		 y1 = temp;  
 		 
-		 return x+","+y;
+		 return "x value: " + x1 +","+ "y value: " +y1;
 	 }
 	 
 	 public String swapLocalPar2(int x, int y)
@@ -110,16 +124,16 @@ public class Swap
 	  * @param index1
 	  * @param index2
 	  */
-	 public void swapList(ArrayList<Integer> q, int index1, int index2)
+	 public void swapList(ArrayList<Integer> arraylist, int index1, int index2)
 	 {
-		 index1 = q.get(1);
-		 index2 = q.get(2);
+		 index1 = arraylist.get(1);
+		 index2 = arraylist.get(2);
 		 
 		 int temp = index1;
 		 index1 = index2;
 		 index2 = temp;
 		 
-		 q.set(0, index1);
-		 q.set(1, index2);
+		 arraylist.set(0, index1);
+		 arraylist.set(1, index2);
 	 }
 }
