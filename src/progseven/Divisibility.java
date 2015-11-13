466ff17c52250b1285fd2f1c60c637e74c4e8d3c
ggrivera <ggrivera@310A31410.district.local>
2015-11-13 10:08:32 -0600

    :P im dead

--- src/progseven/Divisibility.java
+++ src/progseven/Divisibility.java
@@ -1,36 +1,76 @@
 package progseven;
 
+import java.util.Scanner;
+import java.io.*;
+
 /**
  * @author german rivera
- * 
+ * @date 11/13/2015
+ * This program is to calculate the gcd and lcm with int
+ * by using file data.txt.
  *
  */
 public class Divisibility 
 {
 
 	/**
+	 * Test :P
 	 * @param args
 	 */
 	public static void main(String[] args) 
 	{
+		
 		try
 		{
-			gcf.readInt();
+			int[] numbers = readInt();
+			System.out.println("Gcf  ("+numbers[0]+", "+numbers[3]+") ="+gcf(numbers[0], numbers[3]));
+			System.out.println("                                :P                                      ");
+			System.out.println("lcm  ("+numbers[1]+", "+numbers[2]+") ="+lcm(numbers[1], numbers[2]));
 		}
-		catch
+		catch(Exception err)
 		{
-			c.printStackTrace();
+			err.printStackTrace();
 		}
 	}
 	
-	public int readInt()throw Execption
+	/**
+	 * A file reader that returns 4 int that is in an array.
+	 * @return
+	 */
+	public static int[] readInt()throws Exception
 	{
-		c = (FileReader(new FileReader()));
+		FileReader fr = new FileReader("data.txt");
+		BufferedReader br = new BufferedReader(fr);
 		
+		String line;
+		String content = "";
+		
+		while((line = br.readLine()) != null)
+		{
+			content += line;
+		}
+		
+		br.close();
+		
+		String[] stringNumbers = content.split(", ");
+		int[] integerNumbers = new int[stringNumbers.length];
+		
+		for(int x = 0; x < stringNumbers.length; x++)
+		{
+			integerNumbers[x] = Integer.parseInt(stringNumbers[x]);
+		}
+		
+		return integerNumbers;
 		
 	}
 	
-	public int gcd(int a, int b)
+	/**
+	 * Method to calculate the gcd
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public static int gcf(int a, int b)
 	{
 	    while (b > 0)
 	    {
@@ -41,9 +81,15 @@ public class Divisibility
 	    return a;
 	}
 	
-	public int lcm(int a, int b)
+	/**
+	 * Method to calculate the lcm
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public static int lcm(int a, int b)
 	{
-	    return a * (b / gcd(a, b));
+	    return a * (b / gcf(a, b));
 	}
 
 }
