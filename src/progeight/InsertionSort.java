f2839117bd57c5ab5032579915de3d6749bc179e
ggrivera <ggrivera@310A31410.district.local>
2015-11-16 10:15:12 -0600

    starting prog 8 got 25/25 :P

---
+++ src/progeight/InsertionSort.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package progeight;
+
+import java.util.ArrayList;
+/**
+ * @author ggrivera
+ * @Date 11/23/2015
+ */
+public class InsertionSort 
+{
+	private Integer[] array;
+	private static ArrayList<Integer> list;
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		
+	}
+	//BufferReader out;
+	//out = new BufferReader(new FileReader("data.txt"));
+	//out.write(Integer.toString
+	public void Insertionsort(Integer[] array)
+	{
+		
+	}
+	
+	public InsertionSort(ArrayList<Integer> list)
+	{
+		list = new ArrayList<Integer>();
+		
+	}
+	
+	public String toString()
+	{
+		return null;
+		
+	}
+
+}
