704d94de769cd5a196d269dc4f65355c43ebb7b1
ggrivera <ggrivera@310A31408.district.local>
2015-11-19 10:14:11 -0600

    finish by the 30th

--- src/progeight/InsertionSelectionSort.java
+++ src/progeight/InsertionSelectionSort.java
@@ -1,8 +1,6 @@
-/**
- * 
- */
 package progeight;
 
+import java.io.FileReader;
 import java.util.ArrayList;
 
 /**
@@ -19,7 +17,23 @@ public class InsertionSelectionSort
 		
 	}
 	
-	public void insertionSort(Comparable<Integer> A)
+	public InsertionSelectionSort()
+	{
+		arr = new Integer[100];
+		list = new ArrayList<Integer>(100);
+		
+		for(int i = 0; i < 101; i++)
+		{
+			arr[i] = new Integer((int)Math.random());
+		}
+		
+		for(int i = 0; i < 101; i++)
+		{
+			list.add((int) Math.random());
+		}
+	}
+	
+	public void insertionSort(ArrayList<Integer> A)
 	{
 		for (int currentSorted = 1; currentSorted < A.length -1; currentSorted++)
 		{
@@ -57,7 +71,16 @@ public class InsertionSelectionSort
         }
         return arr;
     }
+	
+	public void print(ArrayList a, Comparable b[], String msg) throws Exception
+	{
+		
+		out = new BufferReader(new FileReader("data.txt"));
+		out.write(Integer.toString() )
+		BufferReader out;		
+	}
 //NOTE M's site has some code but hobbled
+
 	
 //	public boolean selectionSort(Comparable<Integer> A[])
 //	{
