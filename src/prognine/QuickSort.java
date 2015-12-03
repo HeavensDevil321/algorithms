6b56a418b752b97dc46cee3e2afb0e5e8558c3b5
ggrivera <ggrivera@310A31411.district.local>
2015-12-03 10:19:52 -0600

    finish but need to understand it!

--- src/prognine/QuickSort.java
+++ src/prognine/QuickSort.java
@@ -5,10 +5,11 @@ package prognine;
  * @date 12/2/2015
  * This program does quick sort in which it grabs the first number of the array and makes
  * it a pivot point swap numbers that are smaller than it to the left and large to the right
- * and continues 
+ * and continues until every thing is sorted
  */
 public class QuickSort 
 {
+	
 	private int array[];
     private int length;
     
@@ -16,9 +17,15 @@ public class QuickSort
     {         
         QuickSort sorter = new QuickSort();
         
-        int[] input = {-1, 5, 17, 22, 1002, 3, -4, 7, 10, 0, 4, 44};
+        int[] input = new int[10];// makes the array of 10
         
-        System.out.print("original array: ");
+        for(int x = 0; x < 10; x++)//makes the array full of random numbers
+        {
+        	int random = (int) (Math.random()*100);
+			input[x] = random;
+        }
+        
+        System.out.print("original array: ");//prints the original array
         for(int i:input)
         {
         	System.out.print(i);
@@ -26,9 +33,9 @@ public class QuickSort
         }
         
         
-        sorter.sort(input);
+        sorter.sort(input);// execute the sort 
         
-        System.out.print("\nSorted array: ");
+        System.out.print("\nSorted array: ");//prints the sorted array
         
         for(int i:input)
         {
@@ -37,12 +44,15 @@ public class QuickSort
         }
     }
     
-    
-    
+    /**
+     * the sort method calls upon the the array to do quick sort
+     * and also checks if it is null or zero
+     * @param inputArr
+     */
     public void sort(int[] inputArr) 
     {
          
-        if (inputArr == null || inputArr.length == 0) 
+        if (inputArr == null || inputArr.length == 0) //depending if one of the conditions is true
         {
             return;
         }
@@ -54,6 +64,9 @@ public class QuickSort
         quickSort(0, length - 1);
     }
     
+    /*
+     * simple swap method for the sort between numbers
+     */
     private void exchangeNumbers(int i, int j) 
     {
         int temp = array[i];
@@ -61,6 +74,11 @@ public class QuickSort
         array[j] = temp;
     }
     
+    /**
+     * 
+     * @param lowerIndex
+     * @param higherIndex
+     */
     private void quickSort(int lowerIndex, int higherIndex) 
     {         
         int i = lowerIndex;
@@ -72,6 +90,7 @@ public class QuickSort
         // Divide into two arrays
         while (i <= j) 
         {
+        	
             /**
              * In each iteration, we will identify a number from left side which
              * is greater then the pivot value, and also we will identify a number
